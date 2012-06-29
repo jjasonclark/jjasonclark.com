37dcd65d6b84142f485d447e88a35854a7a861ed
Jason Clark <jjasonclark@gmail.com>
2012-06-28 21:53:05 -0700

    created turtledev theme and move current files to it upgrade views to 0.9.13

---
+++ themes/turtledev/README.md
@@ -0,0 +1,7 @@
+Turtledev Nesta theme
+=====================
+
+turtledev is a theme for Nesta, a [Ruby CMS](nesta), designed by
+<insert your name here>.
+
+[nesta]: http://nestacms.com
---
+++ themes/turtledev/app.rb
@@ -0,0 +1,19 @@
+# Use the app.rb file to load Ruby code, modify or extend the models, or
+# do whatever else you fancy when the theme is loaded.
+
+module Nesta
+  class App
+    # Uncomment the Rack::Static line below if your theme has assets
+    # (i.e images or JavaScript).
+    #
+    # Put your assets in themes/turtledev/public/turtledev.
+    #
+    # use Rack::Static, :urls => ["/turtledev"], :root => "themes/turtledev/public"
+
+    helpers do
+      # Add new helpers here.
+    end
+
+    # Add new routes here.
+  end
+end
