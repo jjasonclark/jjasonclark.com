04a500586d1f43fba8e61ecdeaf727cfad276f70
Jason Clark <jjasonclark@gmail.com>
2013-04-06 16:23:19 -0700

    Add contact form

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'http://rubygems.org'
 gem 'nesta', "~>0.9.13"
 gem 'ultraviolet', :require => 'uv'
 gem 'rack-codehighlighter', "~>0.5.0", :require => 'rack/codehighlighter'
+gem 'pony'
 
 # gem (RUBY_VERSION =~ /^1.9/) ? 'ruby-debug19': 'ruby-debug'
--- themes/turtledev/app.rb
+++ themes/turtledev/app.rb
@@ -18,6 +18,32 @@ module Nesta
       @page.flagged_as?('commented')
     end
 
+    def send_email(subject, body)
+        Pony.mail :via => 'smtp',
+            :via_options => {
+                :address => 'smtp.mandrillapp.com',
+                :port => '587',
+                :user_name => ENV['MANDRILL_USERNAME'],
+                :password => ENV['MANDRILL_APIKEY'],
+                :authentication => :plain,
+                :domain => 'heroku.com'
+            },
+            :charset => 'utf-8',
+            :to => 'jason@jjasonclark.com',
+            :from => 'webform@jjasonclark.com',
+            :subject => subject,
+            :body => body
+    rescue => e
+        puts "Error while sending email. #{e.class}: #{e.message}"
+    end
+
     # Add new routes here.
+    require 'pony'
+    post '/contact' do
+        subject = "Web feedback: #{Rack::Utils.escape_html(params[:subject])}"
+        body = Rack::Utils.escape_html(params[:body]) || 'No body'
+        send_email subject, body
+        redirect 'thank-you-for-feedback'
+    end
   end
 end
